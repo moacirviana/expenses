2245709693755f8dd74261b9a23948691622e3d8
Moacir Viana <moacir.viana@tre-am.jus.br>
2023-04-26 11:49:07 -0400

    Insert all objects

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,108 @@
 import 'models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-main() => runApp( ExpensesApp());
+main() => runApp(ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
   //const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   //const MyHomePage({super.key});
-   final _transaction = [
-    Transaction(id:'t1',
-            title:'Tênnis de corrida',
-          value: 310.76,
-          date: DateTime.now()
-   ),
-    Transaction(id:'t2',
-            title:'Conta de Luz',
-          value: 120.51,
-          date: DateTime.now()
-   )
-   ];
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
 
-  
+  final _transaction = [
+    Transaction(
+        id: 't1',
+        title: 'Tênnis de corrida',
+        value: 310.76,
+        date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Conta de Luz', value: 120.5, date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Despesas pessoais')),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-              width: double.infinity,
-              child: const Card(
-                color: Colors.blue,
-                elevation: 5,
-                child: Text('Gráfico'),
-              ),
-            ),
+      appBar: AppBar(title: const Text('Despesas pessoais')),
+      body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+        Container(
+          width: double.infinity,
+          child: const Card(
+            color: Colors.blue,
+            elevation: 5,
+            child: Text('Gráfico'),
+          ),
+        ),
         Column(
-          children: _transaction.map((tr){
-            return Card(
+            children: _transaction.map((tr) {
+          return Card(
               child: Row(
+            children: [
+              Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.purple, width: 2)),
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  'R\$ ${tr.value.toStringAsFixed(2)}',
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                      horizontal: 15,
-                      vertical: 10
+                  Text(
+                    tr.title,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
                     ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2
-                      )
-                    ),
-                    padding: const EdgeInsets.all(10),
-                    child: Text(tr.value.toString()),
                   ),
-                  Column(
-                    children: [
-                      Text(tr.title),
-                      Text(tr.date.toString())
-                    ],
-                  )
+                  Text(
+                      style: const TextStyle(color: Colors.grey),
+                      DateFormat('d MMM y').format(tr.date))
                 ],
               )
-            
-            );
-          }).toList()
-         )
+            ],
+          ));
+        }).toList()),
+        Card(
+          elevation: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              children: [
+                TextField(
+                  controller: titleController,
+                  decoration: InputDecoration(labelText: 'Título'),
+                ),
+                TextField(
+                    controller: valueController,
+                    decoration: InputDecoration(labelText: 'R\$')),
+                ElevatedButton(
+                  child: Text('Nova Transação'),
+                  onPressed: () {
+                    print(titleController.text + " " + valueController.text);
+                  },
+                )
+              ],
+            ),
+          ),
+        )
       ]),
     );
-
   }
 }
